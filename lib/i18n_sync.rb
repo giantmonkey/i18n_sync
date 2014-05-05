47da010ddd92b6d581fcd32245e0074168111d55
Adrian Fuhrmann <adrian@giantmonkey.de>
2014-05-05 19:56:15 +0200

    make sure to strip a string

--- lib/i18n_sync.rb
+++ lib/i18n_sync.rb
@@ -7,7 +7,7 @@ class Hash
   def deep_merge!(other_hash)
     other_hash.each do |k,v|
       next unless tv = self[k]
-      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v.strip
+      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v.to_s.strip
     end
     self
   end
