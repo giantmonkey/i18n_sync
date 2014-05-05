675a827dae13bc6bc624f7ab631d98c1bdfaf9d2
Adrian Fuhrmann <adrian@giantmonkey.de>
2014-05-05 19:03:45 +0200

    make sure to strip whitespaces from values

--- lib/i18n_sync.rb
+++ lib/i18n_sync.rb
@@ -12,7 +12,7 @@ class Hash
   def deep_merge!(other_hash)
     other_hash.each do |k,v|
       next unless tv = self[k]
-      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v
+      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v.strip
     end
     self
   end
