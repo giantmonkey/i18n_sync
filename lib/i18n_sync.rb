cfbfd0f81033c177bf0426a0ab1d89c88f5ff46f
Adrian Fuhrmann <adrian@giantmonkey.de>
2014-05-05 20:33:10 +0200

    to not strip

--- lib/i18n_sync.rb
+++ lib/i18n_sync.rb
@@ -7,7 +7,7 @@ class Hash
   def deep_merge!(other_hash)
     other_hash.each do |k,v|
       next unless tv = self[k]
-      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v.to_s.strip
+      self[k] = tv.is_a?(Hash) && v.is_a?(Hash) ? tv.dup.deep_merge!(v) : v
     end
     self
   end
